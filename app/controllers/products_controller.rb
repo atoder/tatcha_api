3e0fdde52bddbf8095dcf817d66f51f122e9f8ee
atoder <alexandert@gmail.com>
2015-05-20 21:15:09 -0700

    Final functionality finished

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,16 @@
 class ProductsController < ApplicationController
   def index
-    response = HTTParty.get('http://api.tatcha.com/shop/api/rest/products?page=1&limit=3&order=name&dir=asc')
+    #default values
+    limit ||= 10
+    @dir = params[:sort] || 'asc'
+    page ||= 1
+    response = HTTParty.get("http://api.tatcha.com/shop/api/rest/products?page=#{page}&limit=#{limit}&order=name&dir=#{@dir}")
     @parsed_response = JSON.parse(response.body)
-    logger.debug "HELLO:" 
-    logger.debug @parsed_response
+  end
+
+  def show
+    @product_id = params[:id]
+    response = HTTParty.get("http://api.tatcha.com/shop/api/rest/products/#{@product_id}")
+    @product = JSON.parse(response.body)
   end
 end
